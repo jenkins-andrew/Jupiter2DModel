b9e510f05407dcb73bb0d832a6b3795f373db1c7
Andrew <jenkins.andrew.thomas@gmail.com>
2019-10-11 14:22:32 +0100

    Created a grid of x, y , z

--- profiles.py
+++ profiles.py
@@ -1,11 +1,12 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
 
 
 def equatorialMagneticField(r, phi):
     B = 1030000.0 * r ** (-3.756 - 0.12 * math.cos(phi - 3.562)) + \
-        (3.797 - 4.612 * math.cos(phi - 0.825) + 0.606 * math.cos(2 * (phi - 0.473)) +
+(3.797 - 4.612 * math.cos(phi - 0.825) + 0.606 * math.cos(2 * (phi - 0.473)) +
          0.847 * math.cos(3 * (phi - 0.913))) * math.exp(-1 * r / 150)
     return B
 
@@ -20,8 +21,18 @@ for r in np.arange(-150, 150):
         y.append(r * math.sin(phi))
         z.append(equatorialMagneticField(r, phi))
 
+x=np.array(x)
+y=np.array(y)
+z=np.array(z)
+xmin, xmax = -100, 100
+ymin, ymax = -100, 100
+xi = np.arange(xmin, xmax, 1)
+yi = np.arange(ymin, ymax, 1)
 
-X, Y =np.meshgrid(x, y)
+xi, yi = np.meshgrid(xi,yi)
+
+zi =griddata((x, y), z, (xi, yi), method='nearest')
 
 fig, ax = plt.subplots()
-CS = ax.contour(X,Y,z)
+
+CS = ax.contour(x,y,zi)
