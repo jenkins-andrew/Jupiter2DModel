ade1aee47e22a87085feef2dd03d25e9bb2faaae
Andrew <jenkins.andrew.thomas@gmail.com>
2019-10-14 11:54:50 +0100

    Changed the way B is calculated. Made directly from an array of x and y

--- profiles.py
+++ profiles.py
@@ -2,43 +2,29 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
-from matplotlib import ticker, cm
+from scipy import interpolate
+
 
 def equatorialMagneticField(r, phi):
-    B = 1.030e6 * r ** (-3.756 - 0.12 * math.cos(phi - 3.562)) + \
-(3.797 - 4.612 * math.cos(phi - 0.825) + 0.606 * math.cos(2 * (phi - 0.473)) +
-         0.847 * math.cos(3 * (phi - 0.913))) * math.exp(-1 * r / 150)
+    B = 1.030e6 * r ** (-3.756 - 0.12 * np.cos(phi - 3.562)) + \
+        (3.797 - 4.612 * np.cos(phi - 0.825) + 0.606 * np.cos(2 * (phi - 0.473)) +
+         0.847 * np.cos(3 * (phi - 0.913))) * np.exp(-1 * r / 150)
     return B
 
 
-x = []
-y = []
-z = []
-
 file = open('Test.txt', 'w')
 
-for r in np.arange(1, 51, 0.5):
-    for phi in np.arange(0, 2 * math.pi, 0.05):
-        x.append(r * math.cos(phi))
-        y.append(r * math.sin(phi))
-        z.append(equatorialMagneticField(r, phi))
+x = np.arange(20, 101, 1)
+y = np.arange(0, 2 * np.pi, 0.1)
 
+xi, yi = np.meshgrid(x, y)
+z = np.array(equatorialMagneticField(xi, yi))
 
-x = np.array(x)
-y = np.array(y)
-z = np.array(z)
+xi = x * np.cos(yi)
+yi = x * np.sin(yi)
 
-xmin, xmax = -50, 50
-ymin, ymax = -50, 50
-xi = np.arange(xmin, xmax, 1)
-yi = np.arange(ymin, ymax, 1)
-
-xi, yi = np.meshgrid(xi,yi)
-
-zi =griddata((x, y), z, (xi, yi))
-
-heatmap= plt.contourf(xi,yi,zi,100, locator=ticker.LogLocator(), cmap=plt.cm.get_cmap('gist_rainbow'),alpha=0.4)
-lines= plt.contour(xi,yi,zi,5,colors='k')
-plt.clabel(lines,fontsize=18, inline=1, colors='k')
+heatmap = plt.contourf(xi, yi, z, 100, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
+lines = plt.contour(xi, yi, z, 5, colors='k')
+plt.clabel(lines, fontsize=18, inline=1, colors='k')
 plt.colorbar(heatmap)
 plt.show()
