e7a9a13d63d2950486fb67ad8c2f915fad08df01
Andrew <jenkins.andrew.thomas@gmail.com>
2019-11-05 12:47:32 +0000

    Created a scale height function

--- creatingProfiles.py
+++ creatingProfiles.py
@@ -6,6 +6,12 @@ from scipy.interpolate import griddata
 from scipy import interpolate
 
 def equatorialMagneticField(r, phi):
+    """
+    Finds the equatorial magnetic field strength using Vogt et. all 2011 method
+    :param r: The radius
+    :param phi: The angle
+    :return: The equatorial magnetic field
+    """
     B = 1.030e6 * r ** (-3.756 - 0.12 * np.cos(phi - 3.562)) + \
         (3.797 - 4.612 * np.cos(phi - 0.825) + 0.606 * np.cos(2 * (phi - 0.473)) +
          0.847 * np.cos(3 * (phi - 0.913))) * np.exp((-1 * r) / 150)
@@ -28,6 +34,12 @@ def corotationVelocityFunc(x, y):
     v = (2*np.pi/36000) * np.sqrt(x**2 + y**2)
     return v
 
+def scaleHeight(r):
+    h = -0.116 + 2.14*np.log10(r) - 2.05*np.log10(r)**2 + 0.491*np.log10(r)**3 + 0.126*np.log10(r)**4
+    H = 10 ** h
+    return H
+
+
 x = []
 y = []
 b = []
@@ -39,7 +51,6 @@ for r in np.arange(1, 150, 1):
         y.append(r * np.sin(phi))
         b.append(equatorialMagneticField(r, phi))
         p.append(plasmaDensity(r, phi))
-
 
 alfvenVelocity = []
 corotationVelocity = []
