8bcd3a56de86649945a9228e7c836019246c3143
Andrew <jenkins.andrew.thomas@gmail.com>
2019-11-07 16:47:44 +0000

    Added a series of subplots to check what the issue is with amu

--- plottingProfiles.py
+++ plottingProfiles.py
@@ -8,24 +8,32 @@ x, y, b, p, alfvenVelocity, corotation, corotationcheck = np.loadtxt('alfvenChec
 r, scaleHeight = np.loadtxt('scaleheighttest.txt', delimiter='\t', unpack=True)
 
 # Creating grid
-xtest = np.arange(-100, 101, 1)
-ytest = np.arange(-100, 101, 1)
+xtest = np.arange(-30, 31, 1)
+ytest = xtest
 xtest, ytest = np.meshgrid(xtest, ytest)
 
 # Masking a circle of radius 20 R_J
-mask = (np.sqrt(xtest ** 2 + ytest ** 2) <= 6) | (np.sqrt(xtest ** 2 + ytest ** 2) >= 100)
+mask = (np.sqrt(xtest ** 2 + ytest ** 2) < 6) | (np.sqrt(xtest ** 2 + ytest ** 2) > 30)
 
 # Making the 3D grid for the magnetic field
-BGrid = griddata((x, y), b, (xtest, ytest), method='linear')
+BGrid = griddata((x, y), b, (xtest, ytest))
 BGrid[mask] = np.nan
 
 # Making the 3D grid for the plasma density
-PGrid = griddata((x, y), p, (xtest, ytest), method='linear')
+PGrid = griddata((x, y), p, (xtest, ytest))
 PGrid[mask] = np.nan
 
 # Making the 3D grid for the Alfven Velocity
-AVGrid = griddata((x, y), alfvenVelocity, (xtest, ytest), method='linear')
+AVGrid = griddata((x, y), alfvenVelocity/1000, (xtest, ytest))
 AVGrid[mask] = np.nan
+
+# Making the 3D grid for the Corotation Velocity
+VGrid = griddata((x, y), corotation/1000, (xtest, ytest))
+VGrid[mask] = np.nan
+
+# Making the 3D grid for the Alfven radius
+CheckGrid = griddata((x, y), corotationcheck, (xtest, ytest))
+CheckGrid[mask] = np.nan
 
 # Plotting
 # plt.subplot(121)
@@ -80,7 +88,65 @@ AVGrid[mask] = np.nan
 # plt.text(10, -70, r'$\rightarrow$ To the Sun')
 # plt.tight_layout()
 
+# plt.figure()
+# alfvenmask = (alfvenVelocity > 0.95*corotation) & (alfvenVelocity < 1.05*corotation)
+# calculatedRadius = np.sqrt(x ** 2 + y ** 2)
+# phiwrong = np.arctan2(x, y)
+# phi = np.mod(phiwrong, 2*np.pi) * 180 / np.pi
+# fit = np.poly1d(np.polyfit(phi[alfvenmask], calculatedRadius[alfvenmask], 3))
+# plt.scatter(phi[alfvenmask], calculatedRadius[alfvenmask], s=0.1, color='k')
+# fitrange = np.arange(0, 360, 1)
+# plt.plot(fit(fitrange))
+# plt.xlabel('Angle (Degrees)', fontsize=18)
+# plt.ylabel('Radius (R$_J)$', fontsize=18)
+# plt.xticks(size=18)
+# plt.yticks(size=18)
+# plt.tight_layout()
+
 plt.figure()
+plt.rcParams['xtick.labelsize'] = 18
+plt.rcParams['ytick.labelsize'] = 18
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.tight_layout()
+ax = plt.subplot(221)
+heatmap = plt.contourf(xtest, ytest, AVGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
+lines = plt.contour(xtest, ytest, AVGrid, 5, colors='k')
+plt.clabel(lines, inline=1, colors='k')
+Jupiter = plt.Circle((0, 0), radius=1, color='k')
+ax.add_artist(Jupiter)
+clb = plt.colorbar(heatmap)
+clb.ax.set_title(r'(kms$^{-1}$)', fontsize=18)
+plt.title('Alfven Velocity', fontsize=18, wrap=True)
+plt.xlabel('x $(R_J)$', fontsize=18)
+plt.ylabel('y $(R_J)$', fontsize=18)
+plt.xticks(size=18)
+plt.yticks(size=18)
+
+ax = plt.subplot(222)
+heatmap = plt.contourf(xtest, ytest, VGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
+lines = plt.contour(xtest, ytest, VGrid, 5, colors='k')
+plt.clabel(lines, inline=1, colors='k')
+Jupiter = plt.Circle((0, 0), radius=1, color='k')
+ax.add_artist(Jupiter)
+clb = plt.colorbar(heatmap)
+clb.ax.set_title(r'(kms$^{-1}$)', fontsize=18)
+plt.title('Corotation Velocity', fontsize=18, wrap=True)
+plt.xlabel('x $(R_J)$', fontsize=18)
+plt.ylabel('y $(R_J)$', fontsize=18)
+plt.xticks(size=18)
+plt.yticks(size=18)
+
+ax = plt.subplot(223)
+lines = plt.contour(xtest, ytest, CheckGrid, 1, colors='k')
+Jupiter = plt.Circle((0, 0), radius=1, color='k')
+ax.add_artist(Jupiter)
+plt.title('Alfven Radius', fontsize=18, wrap=True)
+plt.xlabel('x $(R_J)$', fontsize=18)
+plt.ylabel('y $(R_J)$', fontsize=18)
+plt.xticks(size=18)
+plt.yticks(size=18)
+
+ax = plt.subplot(224)
 alfvenmask = (alfvenVelocity > 0.95*corotation) & (alfvenVelocity < 1.05*corotation)
 calculatedRadius = np.sqrt(x ** 2 + y ** 2)
 phiwrong = np.arctan2(x, y)
@@ -88,12 +154,12 @@ phi = np.mod(phiwrong, 2*np.pi) * 180 / np.pi
 fit = np.poly1d(np.polyfit(phi[alfvenmask], calculatedRadius[alfvenmask], 3))
 plt.scatter(phi[alfvenmask], calculatedRadius[alfvenmask], s=0.1, color='k')
 fitrange = np.arange(0, 360, 1)
-plt.plot(fit(fitrange))
+ax.add_artist(Jupiter)
+plt.title('Alfven Radius', fontsize=18, wrap=True)
 plt.xlabel('Angle (Degrees)', fontsize=18)
 plt.ylabel('Radius (R$_J)$', fontsize=18)
 plt.xticks(size=18)
 plt.yticks(size=18)
-plt.tight_layout()
 
 # plt.figure()
 # heatmap = plt.contourf(xtest, ytest, AVGrid, cmap=plt.cm.get_cmap('gist_rainbow'), alpha=0.4)
