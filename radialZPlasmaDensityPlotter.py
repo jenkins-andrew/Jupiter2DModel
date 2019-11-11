578c07982ac46e7b47959c8b14f03c4d566de1e6
Andrew <jenkins.andrew.thomas@gmail.com>
2019-11-11 16:20:24 +0000

    Removed the magnetic field parts as they do not work in the coordinate system I am using

--- radialZPlasmaDensityPlotter.py
+++ radialZPlasmaDensityPlotter.py
@@ -5,6 +5,7 @@ from scipy.interpolate import griddata
 
 
 r, z, plasmaDensity = np.loadtxt('zPlasmaDensity.txt', delimiter='\t', unpack=True)
+x, y, b, p, alfvenVelocity, corotation, corotationcheck = np.loadtxt('alfvenCheck.txt', delimiter='\t', unpack=True)
 
 maxR = 30
 xtest = np.arange(6, maxR+1, 1)
@@ -17,7 +18,8 @@ plt.figure()
 plt.rcParams['xtick.labelsize'] = 18
 plt.rcParams['ytick.labelsize'] = 18
 heatmap = plt.contourf(xtest, ytest, DensityGrid, cmap=plt.cm.get_cmap('gist_rainbow'), locator=ticker.LogLocator(), alpha=0.4)
-plt.contour(xtest, ytest, DensityGrid, 1, colors='k')
+# lines = plt.contour(xtest, ytest, BGrid, levels, colors='k')
+# plt.clabel(lines, fontsize=18, inline=1, colors='k')
 clb = plt.colorbar(heatmap)
 clb.ax.set_title(r'(cm$^{-3}$)', fontsize=18)
 plt.xlabel('R $(R_J)$', fontsize=18)
