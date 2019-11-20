80d2dffc174630adc46a35493e19b91831ce6e19
Andrew <jenkins.andrew.thomas@gmail.com>
2019-11-20 17:24:17 +0000

    Placed a mask for values smaller than 10^-5

--- radialZPlasmaDensityPlotter.py
+++ radialZPlasmaDensityPlotter.py
@@ -12,7 +12,10 @@ xtest = np.arange(6, maxR+1, 0.5)
 ytest = np.arange(np.amin(z), np.amax(z), 0.1)
 xtest, ytest = np.meshgrid(xtest, ytest)
 
-DensityGrid = griddata((r, z), plasmaDensity, (xtest, ytest))
+mask = (plasmaDensity > 10**(-5))
+
+DensityGrid = griddata((r[mask], z[mask]), plasmaDensity[mask], (xtest, ytest))
+
 
 plt.figure()
 plt.rcParams['xtick.labelsize'] = 18
