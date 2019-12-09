ae6422f0a4122a1d9c90837fb45c90d1ddd7590a
Andrew <jenkins.andrew.thomas@gmail.com>
2019-12-09 13:17:11 +0000

    Added a corotation check

--- radialZPlasmaDensityPlotter.py
+++ radialZPlasmaDensityPlotter.py
@@ -8,7 +8,7 @@ r, z, plasmaDensity, radialVelocityAtZ, alfvenVelocityAtZ = np.loadtxt('zPlasmaD
 xB, yB, zB, BB = np.loadtxt('plotmagfieldlines.txt', delimiter='\t', unpack=True)
 
 
-maxR = 30
+maxR = 100
 xtest = np.arange(6, maxR+1, 0.5)
 ytest = np.arange(np.amin(z), np.amax(z), 0.1)
 xtest, ytest = np.meshgrid(xtest, ytest)
@@ -17,12 +17,15 @@ theta = -10*np.pi/180
 xB = xB*np.cos(theta) - zB*np.sin(theta)
 zB = xB*np.sin(theta) + zB*np.cos(theta)
 
-mask = (plasmaDensity > 10**(-5))
+mask = (plasmaDensity > 10**(-1))
 maskedFieldLines = (np.abs(xB) > 6)
+maskedRadialVelocity = (radialVelocityAtZ < 10**(8))
 
-DensityGrid = griddata((r[mask], z[mask]), plasmaDensity[mask], (xtest, ytest))
+corotationMask = (radialVelocityAtZ < alfvenVelocityAtZ)
+
+DensityGrid = griddata((r[corotationMask], z[corotationMask]), plasmaDensity[corotationMask], (xtest, ytest))
 FieldGrid = griddata((xB, zB), BB, (xtest, ytest))
-radialGrid = griddata((r, z), radialVelocityAtZ, (xtest, ytest))
+radialGrid = griddata((r[maskedRadialVelocity], z[maskedRadialVelocity]), radialVelocityAtZ[maskedRadialVelocity], (xtest, ytest))
 alfvenGrid = griddata((r, z), alfvenVelocityAtZ, (xtest, ytest))
 
 
@@ -50,7 +53,7 @@ plt.rcParams['ytick.labelsize'] = 18
 plt.subplots_adjust(wspace=0.5, hspace=0.5)
 plt.tight_layout()
 
-ax = plt.subplot(121)
+ax = plt.subplot(211)
 heatmap = plt.contourf(xtest, ytest, alfvenGrid, cmap=plt.cm.get_cmap('gist_rainbow'), locator=ticker.LogLocator(), alpha=0.4)
 lines = plt.contour(xtest, ytest, alfvenGrid, 10, colors='k')
 plt.clabel(lines, fontsize=18, inline=1, colors='k')
@@ -61,9 +64,9 @@ plt.xlabel('R $(R_J)$', fontsize=18)
 plt.ylabel('Z $(R_J)$', fontsize=18)
 plt.xticks(size=18)
 plt.yticks(size=18)
-plt.ylim(np.amin(zB), np.amax(zB))
+#plt.ylim(-4, 4)
 
-ax = plt.subplot(122)
+ax = plt.subplot(212)
 heatmap = plt.contourf(xtest, ytest, radialGrid, cmap=plt.cm.get_cmap('gist_rainbow'), locator=ticker.LogLocator(), alpha=0.4)
 lines = plt.contour(xtest, ytest, radialGrid, 10, colors='k')
 plt.clabel(lines, fontsize=18, inline=1, colors='k')
@@ -74,6 +77,6 @@ plt.xlabel('R $(R_J)$', fontsize=18)
 plt.ylabel('Z $(R_J)$', fontsize=18)
 plt.xticks(size=18)
 plt.yticks(size=18)
-plt.ylim(np.amin(zB), np.amax(zB))
+#plt.ylim(-4, 4)
 
 plt.show()
